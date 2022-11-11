4b42bd59426c04ce6502ee790e31b166ffac54a3
Krzysztof Wiatrzyk <krzwiatrzyk@protonmail.com>
2022-11-11 16:29:48 +0100

    init

---
+++ aws/route53.tf
@@ -0,0 +1,36 @@
+locals {
+  iam_role_policy_text=file("${path.module}/iam_roles/cert-manager-route-53.json")
+  iam_role_policy_json=jsonencode(file("${path.module}/iam_roles/cert-manager-route-53.json"))
+}
+
+# data "aws_iam_policy_document" "inline_policy" {
+#   statement {
+#     actions   = ["ec2:DescribeAccountAttributes"]
+#     resources = ["*"]
+#   }
+# }
+
+resource "aws_iam_user" "cert_manager" {
+  name = "cert_manager"
+  path = "/system/"
+
+  tags = {
+    ManagedBy = "Terraform"
+  }
+
+}
+
+resource "aws_iam_access_key" "cert_manager" {
+  user = aws_iam_user.cert_manager.name
+}
+
+resource "aws_iam_user_policy" "cert_manager" {
+  name = "cert_manager"
+  user = aws_iam_user.cert_manager.name
+
+  policy = local.iam_role_policy_text
+}
+
+output "cert_manager_access_key" {
+  value = aws_iam_access_key.cert_manager.encrypted_secret
+}
