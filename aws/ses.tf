502dbd6e6aec6a28b150a7b0e1ddf4ee98372204
Krzysztof Wiatrzyk <krzwiatrzyk@protonmail.com>
2022-12-07 19:25:33 +0100

    feat(normandy): fix cluster

--- aws/ses.tf
+++ aws/ses.tf
@@ -15,3 +15,37 @@ resource "aws_ses_domain_identity_verification" "windkube" {
 
   depends_on = [aws_route53_record.windkube_ses]
 }
+
+resource "aws_iam_user" "ghost_blog" {
+  name = "ghost_blog"
+}
+
+resource "aws_iam_access_key" "ghost_blog" {
+  user = aws_iam_user.ghost_blog.name
+}
+
+data "aws_iam_policy_document" "ses_sender" {
+  statement {
+    actions   = ["ses:SendRawEmail"]
+    resources = ["*"]
+  }
+}
+
+resource "aws_iam_policy" "ses_sender" {
+  name        = "ses_sender"
+  description = "Allows sending of e-mails via Simple Email Service"
+  policy      = data.aws_iam_policy_document.ses_sender.json
+}
+
+resource "aws_iam_user_policy_attachment" "ghost_blog_ses_sender" {
+  user       = aws_iam_user.ghost_blog.name
+  policy_arn = aws_iam_policy.ses_sender.arn
+}
+
+output "ghost_blog_username" {
+  value = aws_iam_access_key.ghost_blog.id
+}
+
+output "ghost_blog_password" {
+  value = aws_iam_access_key.ghost_blog.ses_smtp_password_v4
+}
